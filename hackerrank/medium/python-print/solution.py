ab91cd627843edf8247fc766314afe2067359bda
Ashvini Ingole <ashvinidilipingole08@gmail.com>
2026-09-26 20:07:46 +0530

    ✅ Print Function | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-26

--- hackerrank/medium/python-print/solution.py
+++ hackerrank/medium/python-print/solution.py
@@ -1,9 +1,4 @@
-def is_leap(year):
-    leap = False
-    
-    # Write your logic here
-    if(year % 4 == 0 and year % 100 !=0) or (year % 400 == 0):
-        leap = True
-    
-    return leap
-
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1, n + 1):
+        print(i, end="")
